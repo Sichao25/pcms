211dc19888835f68c94a52f39608574ed6569409
Gangwar <gangwh@life.scorec.rpi.edu>
2026-03-06 12:19:31 -0500

    merged_develop

--- src/pcms/create_field.h
+++ src/pcms/create_field.h
@@ -19,7 +19,8 @@ namespace pcms
 {
 std::unique_ptr<FieldLayout> CreateLagrangeLayout(
   Omega_h::Mesh& mesh, int order, int num_components,
-  CoordinateSystem coordinate_system,);
+  CoordinateSystem coordinate_system,
+  std::string global_id_name="global");
 
 /**
  * \brief Create a binary field on a uniform grid indicating inside/outside mesh
