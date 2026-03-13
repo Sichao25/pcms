01c0fae0088a1e7f2148b95e9d879e0777ef5366
Jacob Merson <mersoj2@rpi.edu>
2026-03-13 04:34:09 +0000

    fix external build
    This PR fixes the external building by:
    1. Installing FindPETSc.cmake
    2. remove the installed CMake exports of the python targets. I could not think of a valid
       usecase where I would want a downstream CMake project to link against
    the python targets.

--- cmake/FindPETSc.cmake
+++ cmake/FindPETSc.cmake
@@ -1,4 +1,4 @@
-find_package(PkgConfig REQUIRED)
+find_package(PkgConfig REQUIRED QUIET)
 if(DEFINED PETSC_DIR)
   if(DEFINED PETSC_ARCH)
     set(ENV{PKG_CONFIG_PATH} "${PETSC_DIR}/${PETSC_ARCH}/lib/pkgconfig:$ENV{PKG_CONFIG_PATH}")
@@ -10,18 +10,7 @@ endif()
 # we give an internal name _petsc
 # so we can fill up the PETSC_VARIABLE based
 # on static or not
-pkg_check_modules(_petsc PETSc)
-
-find_file(_petsc_PC_FILE petsc.pc                                                                                                 
-  PATHS ${_petsc_LIBRARY_DIRS}/../lib/pkgconfig                                                                                   
-        ${_petsc_STATIC_LIBRARY_DIRS}/../lib/pkgconfig                                                                            
-  ENV PKG_CONFIG_PATH                                                                                                             
-  NO_DEFAULT_PATH)                                                                                                                
-if(_petsc_PC_FILE)
-  file(READ "${_petsc_PC_FILE}" _petsc_PC_CONTENTS)
-  message(STATUS "PETSc .pc file: ${_petsc_PC_FILE}")
-  message(STATUS "PETSc .pc contents:\n${_petsc_PC_CONTENTS}")
-endif()
+pkg_check_modules(_petsc PETSc QUIET)
 
 if(_petsc_FOUND AND _petsc_VERSION)
   set(PETSC_VERSION ${_petsc_VERSION})
@@ -30,31 +19,19 @@ endif()
 # note, there are a number of additional properties that
 # can be extracted / set on a target. We just do the basic
 # set for now. See: https://cmake.org/cmake/help/latest/module/FindPkgConfig.html
-if(PCMS_LINK_PETSC_STATIC)
-  message(STATUS "STATIC PETSC BUILD")
+if(PETSC_LINK_STATIC)
   set(PETSC_LIBRARIES ${_petsc_STATIC_LIBRARIES})
   set(PETSC_INCLUDE_DIRS ${_petsc_STATIC_INCLUDE_DIRS})
   set(PETSC_LIBRARY_DIRS ${_petsc_STATIC_LIBRARY_DIRS})
   set(PETSC_LDFLAGS ${_petsc_STATIC_LDFLAGS})
   set(PETSC_LDFLAGS_OTHER ${_petsc_STATIC_LDFLAGS_OTHER})
 elseif(_petsc_FOUND)
-  message(STATUS "SHARED PETSC BUILD")
   set(PETSC_LIBRARIES ${_petsc_LIBRARIES})
   set(PETSC_INCLUDE_DIRS ${_petsc_INCLUDE_DIRS})
   set(PETSC_LIBRARY_DIRS ${_petsc_LIBRARY_DIRS})
   set(PETSC_LDFLAGS ${_petsc_LDFLAGS})
   set(PETSC_LDFLAGS_OTHER ${_petsc_LDFLAGS_OTHER})
 endif()
-message(STATUS "_petsc_LINK_LIBRARIES ${_petsc_LINK_LIBRARIES}")
-message(STATUS "_petsc_LIBRARIES ${_petsc_LIBRARIES}")
-message(STATUS "_petsc_STATIC_LINK_LIBRARIES ${_petsc_STATIC_LINK_LIBRARIES}")
-message(STATUS "_petsc_STATIC_LIBRARIES ${_petsc_STATIC_LIBRARIES}")
-message(STATUS "PETSC_LIBRARIES ${PETSC_LIBRARIES}")
-message(STATUS "PETSC_INCLUDE_DIRS ${PETSC_INCLUDE_DIRS}")
-message(STATUS "PETSC_LIBRARY_DIRS ${PETSC_LIBRARY_DIRS}")
-message(STATUS "PETSC_LDFLAGS ${PETSC_LDFLAGS}")
-message(STATUS "PETSC_LDFLAGS_OTHER ${PETSC_LDFLAGS_OTHER}")
-
 
 if(NOT TARGET PETSc::PETSc)
   add_library(PETSc::PETSc INTERFACE IMPORTED GLOBAL)
