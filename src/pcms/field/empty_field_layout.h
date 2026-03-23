26c96b2ecba2d7a836f4368435cd081ac232f9c5
Jacob Merson <jacob@jacobmerson.com>
2026-03-23 01:09:30 -0400

    Add an "EmptyLayout" this is a replacement for the dummy field adapter used previously

---
+++ src/pcms/field/empty_field_layout.h
@@ -0,0 +1,39 @@
+#ifndef PCMS_EMPTY_FIELD_LAYOUT_H
+#define PCMS_EMPTY_FIELD_LAYOUT_H
+
+#include "pcms/field/field_layout.h"
+#include <Kokkos_Core.hpp>
+
+namespace pcms
+{
+
+class EmptyFieldLayout : public FieldLayout
+{
+public:
+  EmptyFieldLayout();
+
+  int GetNumComponents() const override;
+  LO GetNumOwnedDofHolder() const override;
+  GO GetNumGlobalDofHolder() const override;
+  Rank1View<const bool, HostMemorySpace> GetOwned() const override;
+  GlobalIDView<HostMemorySpace> GetGids() const override;
+  bool IsDistributed() const override;
+  EntOffsetsArray GetEntOffsets() const override;
+  CoordinateView<HostMemorySpace> GetDOFHolderCoordinates() const override;
+  int GetDimension() const override;
+  Rank1View<const LO, HostMemorySpace>
+  GetDOFHolderClassificationDimensions() const override;
+  Rank1View<const LO, HostMemorySpace>
+  GetDOFHolderClassificationIds() const override;
+
+private:
+  Kokkos::View<bool*, HostMemorySpace> owned_;
+  Kokkos::View<GO*, HostMemorySpace> gids_;
+  Kokkos::View<LO*, HostMemorySpace> class_dims_;
+  Kokkos::View<LO*, HostMemorySpace> class_ids_;
+  Kokkos::View<Real**, HostMemorySpace> coords_;
+};
+
+} // namespace pcms
+
+#endif // PCMS_EMPTY_FIELD_LAYOUT_H
